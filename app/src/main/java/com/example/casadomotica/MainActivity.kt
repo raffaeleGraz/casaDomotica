6c72a5ff043a28756ceee3fe469a5ba2308561b8
raffaeleGraz <raffaele.graziani06@gmail.com>
2025-05-05 15:37:31 +0200

    Creata funzione intent per il collegamento main-stazione

--- app/src/main/java/com/example/casadomotica/MainActivity.kt
+++ app/src/main/java/com/example/casadomotica/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.casadomotica
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +14,14 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val btnStazione = findViewById<Button>(R.id.btnStazione)
+        btnStazione.setOnClickListener {
+            intent()
+        }
+    }
+
+    private fun intent(){
+        val intent = Intent(this, Stazione::class.java)
+        startActivity(intent)
     }
 }
