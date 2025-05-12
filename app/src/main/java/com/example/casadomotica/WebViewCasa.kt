d964a44a8120033fd13ce71b32f6618301b60a2a
raffaeleGraz <raffaele.graziani06@gmail.com>
2025-05-12 15:19:45 +0200

    Creata web view casa

---
+++ app/src/main/java/com/example/casadomotica/WebViewCasa.kt
@@ -0,0 +1,19 @@
+package com.example.casadomotica
+
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class WebViewCasa : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_webview_casa)
+
+        val webView = findViewById<WebView>(R.id.WebViewCasa)
+        
+    }
+}
