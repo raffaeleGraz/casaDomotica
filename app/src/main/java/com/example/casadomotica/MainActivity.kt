93e370e45c9e26aa862f29d6018e555b2c10755b
raffaeleGraz <raffaele.graziani06@gmail.com>
2025-05-12 15:06:16 +0200

    Raggiunte tutte le activity in base al proprio pulsante di riferimento

--- app/src/main/java/com/example/casadomotica/MainActivity.kt
+++ app/src/main/java/com/example/casadomotica/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.casadomotica
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,7 +12,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        
+
+        val btnCasa = findViewById<ImageButton>(R.id.btnCasa)
+        btnCasa.setOnClickListener{
+            intent(Casa::class.java)
+        }
+
+        val btnGiardino = findViewById<ImageButton>(R.id.btnGiardino)
+        btnCasa.setOnClickListener{
+            intent(Giardino::class.java)
+        }
+
+        val btnAI = findViewById<ImageButton>(R.id.btnAI)
+        btnCasa.setOnClickListener{
+            intent(AI::class.java)
+        }
+
+
         val btnStazione = findViewById<Button>(R.id.btnStazione)
         btnStazione.setOnClickListener {
             intent(Stazione::class.java)
